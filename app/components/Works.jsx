e6884c234de9d36b435d6c89fbac731ceef13809
riteshk-007 <codeshorts007@gmail.com>
2024-03-06 11:49:08 +0530

    Add Works component to Home page

---
+++ app/components/Works.jsx
@@ -0,0 +1,56 @@
+import Image from "next/image";
+import React from "react";
+
+const Works = () => {
+  const data = [
+    {
+      image: "/image1.png",
+      title: "Make a Decision",
+      description: "It's time to become financially independent",
+    },
+    {
+      image: "/image2.png",
+      title: "Subscribe",
+      description:
+        "Get in depth knowledge of stock market by subscribing our Trading in the zone and Nifty Nitesh Options Courses.",
+    },
+    {
+      image: "/image3.png",
+      title: "Scheduling Process",
+      description:
+        "Join our live classes through google meet link, also you can go through recordings. Also we provide you the Mentor support.",
+    },
+    {
+      image: "/image4.png",
+      title: "Transformation Completed",
+      description: "Novice to Professional Trader Journey start from here.",
+    },
+  ];
+  return (
+    <div className="w-full flex flex-col items-center justify-center gap-2 md:gap-4 p-5 bg-gray-200 dark:bg-black/30">
+      <p className="text-xs md:text-base font-semibold text-gray-700 dark:text-gray-300 text-center uppercase">
+        JOURNEY OF A STUDENT AT Nifty Nitesh
+      </p>
+      <h1 className="text-lg md:text-4xl font-bold">How Does it Works</h1>
+
+      <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 my-5  md:gap-10 flex-wrap justify-center">
+        {data.map((item, index) => (
+          <div
+            key={index}
+            className="flex flex-col mx-4 items-center justify-center gap-2 bg-white dark:bg-black p-2 md:p-5 rounded-lg shadow-md w-60 h-80 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105"
+          >
+            <Image src={item.image} width={150} height={150} alt="image" />
+            <h1 className="text-sm md:text-lg font-bold text-center">
+              {item.title}
+            </h1>
+            <p className="text-xs md:text-sm text-gray-600 dark:text-gray-300 text-center">
+              {item.description}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Works;
--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero";
+import Works from "./components/Works";
 
 export default function Home() {
   return (
     <div className="w-full flex flex-col">
       <Hero />
+      <Works />
     </div>
   );
 }
