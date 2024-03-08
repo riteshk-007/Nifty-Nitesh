afbf4e0fddcb311e47c89782704547d8a2f43b54
riteshk-007 <codeshorts007@gmail.com>
2024-03-08 20:52:33 +0530

    Remove "Blogs" link from Navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -169,14 +169,7 @@ const Navbar = () => {
                   )}
                 </div>
               </li>
-              <li>
-                <Link
-                  href="/blogs"
-                  className="block py-2  text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-gray-500 md:p-0 md:dark:hover:text-gray-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Blogs
-                </Link>
-              </li>
+
               <li>
                 <Link
                   href="/contact"
