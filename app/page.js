1c0d8198117e2f5700d1528e1c8b5105d979ce2d
riteshk-007 <codeshorts007@gmail.com>
2024-03-20 13:00:27 +0530

    Add StickyScrollRevealDemo component and update framer-motion version

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ import Journey from "./components/Journey";
 import Location from "./components/Location";
 import Slider from "./components/Slider";
 import Works from "./components/Works";
+import { StickyScrollRevealDemo } from "./components/Program";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <Hero />
       <Works />
       <Journey />
+      <StickyScrollRevealDemo />
       <Cards />
       <About />
       <FaqQuesAns numQuestions={5} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,16 @@
+const flattenColorPalette =
+  require("tailwindcss/lib/util/flattenColorPalette").default;
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
@@ -73,5 +86,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), addVariablesForColors],
 };
