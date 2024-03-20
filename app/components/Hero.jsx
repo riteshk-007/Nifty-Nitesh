0c50a4e457c91f2019c34af9a07dc0015d4f9946
riteshk-007 <codeshorts007@gmail.com>
2024-03-20 12:51:26 +0530

    Refactor Hero component and add carousel functionality

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,10 +1,48 @@
-import Image from "next/image";
+"use client";
+import * as React from "react";
+import Autoplay from "embla-carousel-autoplay";
 
+import Image from "next/image";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+
+const images = [
+  {
+    src: "/banner1.png",
+  },
+  {
+    src: "/banner2.png",
+  },
+];
 const Hero = () => {
+  const plugin = React.useRef(
+    Autoplay({ delay: 3000, stopOnInteraction: true })
+  );
   return (
-    <div className="w-full items-center justify-center flex">
-      <Image src="/banner.png" alt="hero" width={1920} height={1080} />
-    </div>
+    <Carousel
+      plugins={[plugin.current]}
+      className="w-full"
+      onMouseEnter={plugin.current.stop}
+      onMouseLeave={plugin.current.reset}
+    >
+      <CarouselContent>
+        {images?.map((item, index) => (
+          <CarouselItem key={index}>
+            <div className="w-full  shadow-lg">
+              <Image
+                width={1920}
+                height={1080}
+                src={item.src}
+                alt={`Slide ${index + 1}`}
+              />
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+    </Carousel>
   );
 };
 
